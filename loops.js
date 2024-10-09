4d424d9923037ddc400df530e390fe745fd1cd08
Jonah <38752@ma-web.nl>
2024-10-09 15:23:15 +0200

    update

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ for(let table = 0; table < 51; table++){
     console.log("table of " + table)
 
     //adds +1 to times till its done with 10
-    for(let times = 0; times < 11; times++){
+    for(let times = 0; times < 51; times++){
         //counts table X times
         let answer = times * table
         console.log(table + " x " + times + " = " + answer)
